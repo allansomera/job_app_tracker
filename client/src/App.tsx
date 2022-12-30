c32e255288e83e1f8fb22b5bc8324fe04f432f4e
Allan Somera <nagal.somera@gmail.com>
2022-12-29 22:15:13 -0800

    using CreatableSelect from bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,23 @@ import reactLogo from "./assets/react.svg"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Navigate, Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import NewNote from "./components/new-note/NewNote"
 
 const App = () => {
   const [count, setCount] = useState(0)
   return (
-    <Routes>
-      <Route path="/" element={<h1>Hi </h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
-      <Route path=":id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Show</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/" element={<h1>Hi </h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
 export default App
---
+++ client/src/components/new-note/NewNote.tsx
@@ -0,0 +1,13 @@
+import NoteForm from "../note-form/NoteForm"
+
+const NewNote = () => {
+  const test = () => {}
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </>
+  )
+}
+
+export default NewNote
